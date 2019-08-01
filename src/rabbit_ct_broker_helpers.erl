29d020754ce34aa28cc398d0755e17d9d876422c
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2019-08-01 20:51:02 +0200

    Set `$RABBITMQ_SCRIPTS_DIR` when calling CLI
    This helps in situations where rabbitmq-env guesses it wrong. I saw this
    situation in the `feature_flags_SUITE` testsuite of rabbitmq-server, but
    couldn't really explain why the guess was wrong.

--- src/rabbit_ct_broker_helpers.erl
+++ src/rabbit_ct_broker_helpers.erl
@@ -951,6 +951,7 @@ rabbitmqctl(Config, Node, Args) ->
     NodeConfig = get_node_config(Config, Node),
     Nodename = ?config(nodename, NodeConfig),
     Env0 = [
+      {"RABBITMQ_SCRIPTS_DIR", filename:dirname(Rabbitmqctl)},
       {"RABBITMQ_PID_FILE", ?config(pid_file, NodeConfig)},
       {"RABBITMQ_MNESIA_DIR", ?config(mnesia_dir, NodeConfig)},
       {"RABBITMQ_PLUGINS_DIR", ?config(plugins_dir, NodeConfig)},
@@ -981,6 +982,7 @@ rabbitmq_queues(Config, Node, Args) ->
     NodeConfig = rabbit_ct_broker_helpers:get_node_config(Config, Node),
     Nodename = ?config(nodename, NodeConfig),
     Env0 = [
+      {"RABBITMQ_SCRIPTS_DIR", filename:dirname(RabbitmqQueues)},
       {"RABBITMQ_PID_FILE", ?config(pid_file, NodeConfig)},
       {"RABBITMQ_MNESIA_DIR", ?config(mnesia_dir, NodeConfig)},
       {"RABBITMQ_PLUGINS_DIR", ?config(plugins_dir, NodeConfig)},
@@ -1621,6 +1623,7 @@ plugin_action(Config, Node, Args) ->
     NodeConfig = get_node_config(Config, Node),
     Nodename = ?config(nodename, NodeConfig),
     Env = [
+      {"RABBITMQ_SCRIPTS_DIR", filename:dirname(Rabbitmqplugins)},
       {"RABBITMQ_PID_FILE", ?config(pid_file, NodeConfig)},
       {"RABBITMQ_MNESIA_DIR", ?config(mnesia_dir, NodeConfig)},
       {"RABBITMQ_PLUGINS_DIR", ?config(plugins_dir, NodeConfig)},
